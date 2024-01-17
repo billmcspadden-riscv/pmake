cf072a6a2c9decea0a14a52b0812d73a6e76193f
William McSpaddden <bill@riscv.org>
2024-01-16 20:04:11 -0600

    test and checking are in place.  command line setting of pmake variables is beginning to work

--- TEST_DIR_ROOT/command_line_options.testdir/setting_variable_from_command_line.testdir/Makefile.py
+++ TEST_DIR_ROOT/command_line_options.testdir/setting_variable_from_command_line.testdir/Makefile.py
@@ -1,5 +1,16 @@
 #! /usr/bin/env python3
 
+# TODO: replace the following with ifdef.   Can do?
+#   GNU make does something like:
+#       ifndef <var>
+#           <var> := <something>
+#       endif
+#   GNU make also  has ...
+#       <var> ?= <something>
+#   ... where <var> is set to <something> if <var> does not exist
+#   How to replicate in pmake?
+
+
 try:
     COMMAND_LINE_VARIABLE
 except NameError:
@@ -7,6 +18,14 @@ except NameError:
     COMMAND_LINE_VARIABLE = None
 else:
     echo("COMMAND_LINE_VARIABLE: " + COMMAND_LINE_VARIABLE)
+
+try:
+    LOGFILE
+except NameError:
+    echo("error: the command line variable, 'LOGFILE', was not set")
+    LOGFILE = "logfile_set_by_pmake.txt"
+else:
+    echo("LOGFILE: " + LOGFILE, ">>", LOGFILE)
 
 
 
